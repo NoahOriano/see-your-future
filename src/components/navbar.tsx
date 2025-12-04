d6db3ef3f59150e3d9d8ff16c13035692fccbbab
NoahOriano <noahoriano@gmail.com>
2025-12-04 13:46:28 -0600

    formatting and navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,9 @@
+import Logo from "../assets/Logo.png";
+
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h2>My App</h2>
+      <img src={Logo} alt="logo" style={{height: "124px"}}/>
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
